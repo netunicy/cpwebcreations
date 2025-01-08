ead63f8bd65694e9e4e2e892f09bd91a4b96ba8a
Pitris Charalambos <charalampospitris1983@gmail.com>
2025-01-08 22:56:47 +0200

    ok

--- cpwebcreations/cpwebcreations/settings.py
+++ cpwebcreations/cpwebcreations/settings.py
@@ -108,10 +108,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
