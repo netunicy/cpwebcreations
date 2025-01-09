2d1507383c17452fdda2588e511109517bc43922
Pitris Charalambos <charalampospitris1983@gmail.com>
2025-01-09 20:39:33 +0200

    ok

--- cpwebcreations/cpwebcreations/urls.py
+++ cpwebcreations/cpwebcreations/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cpsoftware.urls')),
     path('tinymce/', include('tinymce.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/staticfiles/favico/favicon.ico')),
 ]
