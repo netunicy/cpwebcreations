ee2cd796b8dbfe2a410e96a8db93b2cefeb25455
Pitris Charalambos <charalampospitris1983@gmail.com>
2025-01-18 05:26:37 +0200

    ok

--- cpwebcreations/cpwebcreations/urls.py
+++ cpwebcreations/cpwebcreations/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('captcha/', include('captcha.urls')),
     path('favicon.ico', RedirectView.as_view(url='/staticfiles/favico/favicon.ico')),
-    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
