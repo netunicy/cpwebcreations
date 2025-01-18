f9470f55918ddc24249e0beb1722729f2d2dc747
Pitris Charalambos <charalampospitris1983@gmail.com>
2025-01-18 05:03:19 +0200

    ok

--- cpwebcreations/cpwebcreations/urls.py
+++ cpwebcreations/cpwebcreations/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('captcha/', include('captcha.urls')),
     path('favicon.ico', RedirectView.as_view(url='/staticfiles/favico/favicon.ico')),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
