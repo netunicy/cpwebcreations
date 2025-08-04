96a6b58518a5b00c6abca8df3f3501f7ea042c03
Pitris Charalambos <charalampospitris1983@gmail.com>
2025-08-04 22:55:36 +0100

    update-email

--- cpwebcreations/cpsoftware/views.py
+++ cpwebcreations/cpsoftware/views.py
@@ -49,8 +49,8 @@ def contact_us(request):
                 # Δημιουργία email
                 mail = mt.Mail(
                     sender=mt.Address(email="hello@cpsoftwarecreation.com", name="Contact Us"),
-                    to=[mt.Address(email="cpsoftwarecreation@outlook.com")],
-                    bcc=[mt.Address(email="charalampospitris1983@gmail.com")],
+                    to=[mt.Address(email="charalampospitris1983@gmail.com")],
+                    #bcc=[mt.Address(email="charalampospitris1983@gmail.com")],
                     subject=subject,
                     text=f"{name} {surname}\n{phone}\n{email}\n{message}",
                     category="Contact Us"
